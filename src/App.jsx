b769c5077558c162b27e916b8abea885ee264d23
Lesya Kudryashova <lkudryashova.work@gmail.com>
2025-02-23 09:09:39 +0100

    HW04 update

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,80 @@
-import ContactForm from "./components/ContactForm/ContactForm";
-import SearchBox from "./components/SearchBox/SearchBox";
-import ContactList from "./components/ContactList/ContactList";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import ImageModal from "./components/ImageModal/ImageModal";
+import Loader from "./components/Loader/Loader";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import SearchBar from "./components/SearchBar/SearchBar";
 import { useState, useEffect } from "react";
-
-const initialContacts = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
+import { Toaster, toast } from "react-hot-toast";
+import * as imagesService from "./services/api";
 
 export default function App() {
-  const [contacts, setContacts] = useState(() => {
-    const savedContacts = localStorage.getItem("contacts");
-    return savedContacts ? JSON.parse(savedContacts) : initialContacts;
-  });
-
-  const [filter, setFilter] = useState("");
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   useEffect(() => {
-    localStorage.setItem("contacts", JSON.stringify(contacts));
-  }, [contacts]);
+    if (!query) return;
+    const getData = async () => {
+      setLoading(true);
+      try {
+        const response = await imagesService.fetchImages(query, page);
+        setImages((prev) => [...prev, ...response.results]);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, [query, page]);
 
-  const addContact = (newContact) => {
-    setContacts((prev) => {
-      return [...prev, newContact];
-    });
+  const handleSearch = (newQuery) => {
+    if (newQuery === "") {
+      toast.error("Please enter a search term");
+      return;
+    }
+    setQuery(newQuery);
+    setImages([]);
+    setPage(1);
+    setError(null);
   };
 
-  const deleteContact = (id) => {
-    setContacts((prev) => prev.filter((contact) => contact.id !== id));
+  const handleLoadMore = () => {
+    setPage((prev) => prev + 1);
   };
 
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const openModal = (image) => {
+    setSelectedImage(image);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null);
+    setShowModal(false);
+  };
 
   return (
-    <div className="wrapper">
-      <h1>Phonebook</h1>
-      <ContactForm onSubmit={addContact} />
-      <SearchBox filter={filter} setFilter={setFilter} />
-      <ContactList contacts={filteredContacts} onDelete={deleteContact} />
+    <div className="container">
+      <SearchBar onSubmit={handleSearch} />
+      {error && <ErrorMessage message={error} />}
+      <ImageGallery images={images} openModal={openModal} />
+      {loading && (
+        <div className="loader-wrapper">
+          <Loader />
+        </div>
+      )}
+      {images.length > 0 && !loading && (
+        <LoadMoreBtn onClick={handleLoadMore} />
+      )}
+      {showModal && (
+        <ImageModal selectedImage={selectedImage} closeModal={closeModal} />
+      )}
+      <Toaster />
     </div>
   );
 }
